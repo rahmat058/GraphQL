893f32c39080f522de1ef5273d2cca9f88109545
Kazi Rahamatullah <md.kazi.rahmat@gmail.com>
2019-10-24 16:53:38 +0600

    Connecting MLab Database is done

--- server/app.js
+++ server/app.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const mongoose = require('mongoose');
 
 const schema = require('./schema/schema');
 
 const app = express();
+
+// connect to mlab database
+// make sure to replace my db string & creds with your own
+mongoose.connect('mongodb://rahmat:test123@ds345937.mlab.com:45937/gql')
+mongoose.connection.once('open', () => {
+    console.log('conneted to database');
+});
 
 app.use('/graphql', graphqlHTTP({
   schema,
