c5bdac117615f82f35949c25030a3a3d84a87b48
rahmat058 <md.kazi.rahmat@gmail.com>
2019-10-25 10:18:47 +0600

    Display data from front-end side is done

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -11,13 +11,25 @@ const getBooksQuery = gql `
   }
 `
 class BookList extends Component {
-  render() {
-    console.log('PRops', this.props);
-    
+  displayBooks() {
+    const data = this.props.data;
+
+    if(data.loading) {
+      return(<div>Loading books...</div>)
+    } else {
+      return data.books.map(book => {
+        return (
+          <li key={book.id}>{ book.name }</li>
+        )
+      })
+    }
+  }
+
+  render() {    
     return (
       <div>
         <ul id="book-list">
-          <li>Book name</li>
+          {this.displayBooks()}
         </ul>
       </div>
     );
