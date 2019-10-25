96e93cd2a22dc9651604aadc669e41201fdfc881
rahmat058 <md.kazi.rahmat@gmail.com>
2019-10-25 19:34:12 +0600

    Single Query is done

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
-import { gql } from 'apollo-boost';
 import { graphql } from 'react-apollo';
 
 import { getBooksQuery } from '../queries/queries';
 import BookDetails from './BookDetails';
 class BookList extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selected: null
+    }
+  }
 
   displayBooks() {
     const data = this.props.data;
@@ -14,7 +21,7 @@ class BookList extends Component {
     } else {
       return data.books.map(book => {
         return (
-          <li key={book.id}>{ book.name }</li>
+          <li key={book.id} onClick={(e) => {this.setState({selected: book.id})} }>{ book.name }</li>
         )
       })
     }
@@ -27,7 +34,7 @@ class BookList extends Component {
           {this.displayBooks()}
         </ul>
 
-        <BookDetails />
+        <BookDetails bookId={this.state.selected} />
       </div>
     );
   }
